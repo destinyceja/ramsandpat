4e6ed5c0dccccd75f1be9cc0eb94a48b64259d2a
destinyceja <33135802+destinyceja@users.noreply.github.com>
2019-01-25 15:41:08 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+	
+// "el" stands for the "element" that was just clicked
+function allOpaque(el){
+	document.getElementById("something").classList.add("opaque");
+	document.getElementById("super").classList.add("opaque");
+	
+}
+
+// "el" stands for the "element" that was just clicked
+function removeColor(el){
+	document.getElementById("pats-column").classList.remove("pats-silver");
+	document.getElementById("rams-column").classList.remove("rams-blue");
+	
+}
+
+function showtrophy(el){
+	document.getElementById('bye').src = "images/1trophy.png";
+	document.getElementById('hey').src = "images/1trophy.png";
+	document.getElementById('11trophy').src = "images/1trophy.png";
+}
